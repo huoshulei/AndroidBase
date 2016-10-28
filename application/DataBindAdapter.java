a923719ec3100cdbae291f517a1dea326b881c21
huoshulei <hsl_4444@163.com>
2016-10-28 10:45:15 +0800

    0

--- application/DataBindAdapter.java
+++ application/DataBindAdapter.java
@@ -1,9 +1,15 @@
 package com.example.icogn.mshb;
 
 import android.databinding.BindingAdapter;
+import android.databinding.BindingMethod;
+import android.databinding.BindingMethods;
+import android.support.v4.widget.NestedScrollView;
+import android.text.TextUtils;
 import android.widget.ImageView;
 
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.display.CircleBitmapDisplayer;
 
 /**
  * 项目名称:  MSHB
@@ -15,11 +21,33 @@ import com.nostra13.universalimageloader.core.ImageLoader;
  * 备注:
  * 版本:
  */
-
+@BindingMethods({
+        @BindingMethod(type = NestedScrollView.class, attribute = "android:scrollY", method = "setOnScrollChangeListener")
+})
 public class DataBindAdapter {
     @BindingAdapter("android:src")
     public static void setImageUrl(ImageView view, String url) {
         ImageLoader.getInstance().displayImage(url, view);
     }
 
+    @BindingAdapter(" app:circle")
+    public static void setCircleImage(ImageView view, String url) {
+        if (TextUtils.isEmpty(url)) return;
+        ImageLoader.getInstance().displayImage(url, view, options);
+    }
+
+    @BindingAdapter("android:src")
+    public static void setImageRes(ImageView view, int resId) {
+        view.setImageResource(resId);
+    }
+
+
+    /**
+     * 设置圆形头像
+     */
+    private static DisplayImageOptions options = new DisplayImageOptions.Builder()
+            .cacheInMemory(true)
+            .cacheOnDisk(true)
+            .displayer(new CircleBitmapDisplayer())
+            .build();
 }
